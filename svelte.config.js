e9687dd5b7a81cddd27472cb62532654efa658b8
Oaj Bidnurkar <oajrockstar@gmail.com>
2024-07-25 12:34:57 +0530

    Pathfix

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    })
-  },
-  preprocess: vitePreprocess()
+    kit: {
+        adapter: adapter({
+            pages: "build",
+            assets: "build",
+            fallback: null,
+            paths: {
+                base: process.env.NODE_ENV === "production" ? "/betterwork-ai" : "",
+            },
+        }),
+    },
+    preprocess: vitePreprocess(),
 };
 
 export default config;
