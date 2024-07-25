1bb844eee3cb403314c385bab32c2f6850223aa5
Oaj Bidnurkar <oajrockstar@gmail.com>
2024-07-25 17:14:25 +0530

    sexier ui

--- src/components/main.svelte
+++ src/components/main.svelte
@@ -1,16 +1,16 @@
 <main class="flex flex-col min-h-full bg-gray-1000 text-white" style="padding-top:30px ;">
     <section class="relative flex h-[100vh] shrink-0 flex-col items-center justify-center">
-        <video class="absolute -z-10 w-full h-full object-cover"muted autoplay loop playsinline disablepictureinpicture>
+        <video class="absolute -z-10 w-full h-full object-cover rounded-3xl"muted autoplay loop playsinline disablepictureinpicture onloadstart="this.playbackRate = 1.75;">
             <source src="./retro.webm" type="video/webm"/>
             <source src="./retro.mp4" type="video/mp4"/>
         </video>
-        <section id="intro" class="flex flex-col items-center justify-center flex-grow text-center px-4">
-            <h2 class="text-5xl font-bold mb-4">We're betterwork <span class="text-violet-400">AI</span></h2>
-            <p class="text-2xl text-blue-400 mb-8" style="padding-top:12px ;">
-                Building web tools because <span class="text-violet-400">'404: Not Found'</span> is never an option!
+        <section id="intro" class="flex flex-col items-center justify-center text-center px-4 backdrop-blur-sm rounded-3xl py-10">
+            <h2 class="text-5xl font-bold mb-4 tracking-wide">We're betterwork.<span class="text-violet-400">AI</span></h2>
+            <p class="text-2xl text-blue-400 mb-8 tracking-widest" style="padding-top:12px ;">
+                Building web tools because <span class="text-violet-400 bg-slate-700 bg-opacity-50 rounded-lg p-1"><code class="tracking-wide">404: Not Found</code></span> is never an option.
             </p>
             
-            <div class="w-full max-w-md" style="padding-top:20px; padding-bottom:80px ;">
+            <div class="w-full max-w-md pt-5 pb-5">
                 <form class="flex flex-col items-center gap-4">
                     <div>
                         <a href="https://forms.gle/MzbAwGPbGgKunPEZ8" target="_blank"  class="px-6 py-2 bg-transparent border-2 border-blue-400 text-blue-400 rounded-full hover:bg-blue-400 hover:text-white transition-colors cursor-pointer">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily : {
-        'Roboto': ['Roboto', 'sans-serif'],
-        'sans': ['Poppins', 'sans-serif'],
-        'serif': ['Poppins', 'serif'],
-        'mono': ['Poppins', 'monospace']
-      }
-    }
-  },
-  plugins: []
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Roboto: ["Roboto", "sans-serif"],
+                sans: ["Poppins", "sans-serif"],
+                serif: ["Poppins", "serif"],
+                mono: ["Poppins", "monospace"],
+                inter: ["Inter", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
 };
