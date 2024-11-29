9943f72e553eb7cdfb16c936ac08bfadf63af93d
Heramb.0x1 <136613738+Heramb0x1@users.noreply.github.com>
2024-11-30 02:21:20 +0530

    Added open-in-sh-v0.1

--- src/components/main.svelte
+++ src/components/main.svelte
@@ -11,8 +11,35 @@
             <source src="./retro.webm" type="video/webm"/>
             <source src="./retro.mp4" type="video/mp4"/>
         </video>
-        <section id="intro" class="flex flex-col items-center justify-center text-center px-4 backdrop-blur-sm rounded-3xl py-10">
-            <h2 class="text-5xl font-bold mb-4 tracking-wide">We're betterwork.<span class="text-violet-400">ai</span></h2>
+        <section id="intro" class="flex flex-col items-center justify-center text-center px-4 pt-0 backdrop-blur-sm rounded-3xl py-10">
+            <div style=" padding-bottom: 48px;">
+                <a href="https://git.new/open-in-sci-hub" 
+                target="_blank" 
+                class="group relative inline-flex items-center justify-start overflow-hidden rounded bg-indigo-50 py-3 pl-6 pr-16 font-semibold text-indigo-600 transition-all duration-150 ease-in-out hover:pl-16 hover:pr-6">
+                <span class="absolute bottom-0 left-0 h-1 w-full bg-indigo-600 transition-all duration-150 ease-in-out group-hover:h-full"></span>
+                
+                <span class="absolute right-0 pr-7 duration-200 ease-out group-hover:translate-x-12">
+                    <svg class="h-5 w-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                    </svg>
+                </span>
+                
+                <span class="absolute left-0 -translate-x-12 pl-7 duration-200 ease-out group-hover:translate-x-0">
+                    <svg class="h-5 w-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                    </svg>
+                </span>
+                
+                <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                    Try `open-in-Sci-Hub`
+                </span>
+            </a>
+            </div>
+
+
+
+
+            <h2 class="text-5xl font-bold mb-4 tracking-wide pt-14">We're betterwork.<span class="text-violet-400">AI</span></h2>
             <p class="text-2xl text-blue-400 mb-8 tracking-widest" style="padding-top:12px ;">
                 Building web tools because <span class="text-violet-400 bg-slate-700 bg-opacity-50 rounded-lg p-1"><code class="tracking-wide">404: Not Found</code></span> is never an option.
             </p>
@@ -20,13 +47,25 @@
             <div class="w-full max-w-md pt-5 pb-5">
                 <form class="flex flex-col items-center gap-4">
                     <div>
-                        <a href="https://forms.gle/MzbAwGPbGgKunPEZ8" target="_blank"  class="px-6 py-2 bg-transparent border-2 border-blue-400 text-blue-400 rounded-full hover:bg-blue-400 hover:text-white transition-colors cursor-pointer">
+                        <a href="https://forms.gle/MzbAwGPbGgKunPEZ8" target="_blank" class="button-link block w-full text-center px-12 py-2   bg-transparent border-2 border-blue-400 text-blue-400 rounded-full hover:bg-blue-400 hover:text-white transition-colors duration-300 relative ">
                             Join Waitlist
+                            <span class="arrow-hover inline-block ml-2">
+                                →
+                            </span>
                         </a>
                     </div>
+
                 </form>
             </div>
         </section>
     </section>
+    <style>
+        .arrow-hover {
+            transition: transform 0.3s ease;
+        }
+        .button-link:hover .arrow-hover {
+            transform: translateX(10px);
+        }
+    </style>
 </main>
 
