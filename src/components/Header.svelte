c8172d21aa53ed7204615322c1e148336f471890
8010488823 <alphagoku.28.06@gmail.com>
2024-12-12 18:33:18 +0530

    colour aspect ratio fixes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -49,7 +49,7 @@
         <a
             href="https://x.com/betterworkAI"
             target="_blank"
-            class="blueShadow px-4 py-2 rounded-2xl bg-violet-400 text-white flex items-center justify-center"
+            class="blueShadow px-4 py-2 rounded-2xl bg-violet-400 flex items-center justify-center"
         >
             <img src="/X-twitter-logo.png" alt="𝕏" class="h-5 w-5" />
         </a>
@@ -57,7 +57,7 @@
         <a
             href="https://www.linkedin.com/company/betterwork-ai/"
             target="_blank"
-            class="blueShadow px-4 py-2 rounded-2xl bg-violet-400 text-white flex items-center justify-center"
+            class="blueShadow px-4 py-2 rounded-2xl bg-violet-400 flex items-center justify-center"
         >
             <img src="/linkedin-logo.png" alt="in" class="h-5 w-5" />
         </a>
