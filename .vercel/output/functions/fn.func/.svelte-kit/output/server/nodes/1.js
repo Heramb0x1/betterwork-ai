09393e3cfc54490694b3335f7d05caaef5606549
8010488823 <alphagoku.28.06@gmail.com>
2025-01-21 09:12:33 +0530

    about-page-update

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CQPisQRJ.js","_app/immutable/chunks/scheduler.DuLnc7_I.js","_app/immutable/chunks/index.BXGASEWR.js","_app/immutable/chunks/entry.Bba1vrJo.js"];
+export const imports = ["_app/immutable/nodes/1.sx3dEdNU.js","_app/immutable/chunks/scheduler.DuLnc7_I.js","_app/immutable/chunks/index.BXGASEWR.js","_app/immutable/chunks/entry.B4yXytu5.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.BMtK1h7A.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Bba1vrJo.js";export{t as start};
